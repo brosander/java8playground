13efb4c9286c4b587933c9fe18c5004da6630e76
brosander <brosander@pentaho.com>
2016-04-06 10:06:58 -0400

    adding parallel sort examples

--- src/test/java/com/github/java8playground/ParallelArrayDemo.java
+++ src/test/java/com/github/java8playground/ParallelArrayDemo.java
@@ -3,60 +3,84 @@ package com.github.java8playground;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
-import java.util.stream.IntStream;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 /**
  * Created by bryan on 4/5/16.
  */
 public class ParallelArrayDemo {
+    private Comparator<Integer> makeComparator(int expensiveRuns) {
+        return (o1, o2) -> {
+            String myString = "";
+            for (int i = 0; i < expensiveRuns; i++) {
+                myString += "expensive concatenation ";
+            }
+            return Integer.compare(o1, o2);
+        };
+    }
+
     @Test
-    public void testParallelArraySort() {
+    public void testNoComparator() {
+        benchmark(1_000_000, 10, 25, Arrays::sort, Arrays::parallelSort);
+    }
+
+    @Test
+    public void testExpensiveComparator() {
+        Comparator<Integer> expensiveComparator = makeComparator(5);
+        benchmark(100_000, 5, 10, integers -> Arrays.sort(integers, expensiveComparator), integers -> Arrays.parallelSort(integers, expensiveComparator));
+    }
+
+    @Test
+    public void testCheapComparator() {
+        Comparator<Integer> expensiveComparator = makeComparator(0);
+        benchmark(100_000, 5, 10, integers -> Arrays.sort(integers, expensiveComparator), integers -> Arrays.parallelSort(integers, expensiveComparator));
+    }
+
+    public void benchmark(int arraySize, int warmupRuns, int benchmarkRuns, Consumer<Integer[]> baseline, Consumer<Integer[]> test) {
         Random random = new Random();
-        int size = 1_000_000;
-        int warmupNumber = 10;
-        int runNumber = 50;
-        int numOccurrences = (warmupNumber + runNumber) * 2;
+        int numOccurrences = (warmupRuns + benchmarkRuns) * 2;
 
-        System.out.println("Array size: " + size);
-        System.out.println("Warmup number: " + warmupNumber);
-        System.out.println("Run number: " + runNumber);
+        System.out.println("Array size: " + arraySize);
+        System.out.println("Warmup number: " + warmupRuns);
+        System.out.println("Run number: " + benchmarkRuns);
 
-        System.out.println("Generating " + (1 + warmupNumber + runNumber) + " arrays of size " + size);
+        System.out.println("Generating " + (1 + warmupRuns + benchmarkRuns) + " arrays of size " + arraySize);
         long beforeArrays = System.currentTimeMillis();
-        int[] original = IntStream.generate(random::nextInt).limit(size).toArray();
-        int[][] copies = Stream.generate(() -> Arrays.copyOf(original, original.length)).limit(numOccurrences).toArray(value -> new int[numOccurrences][]);
+        Integer[] original = Stream.generate(random::nextInt).limit(arraySize).toArray(Integer[]::new);
+        Integer[][] copies = Stream.generate(() -> Arrays.copyOf(original, original.length)).limit(numOccurrences).toArray(value -> new Integer[numOccurrences][]);
         System.out.println("Finished generating arrays after " + (System.currentTimeMillis() - beforeArrays) / 1000.0 + " seconds");
 
-        System.out.println("Starting " + warmupNumber + " warmup sorts of each type");
+        System.out.println("Starting " + warmupRuns + " warmup sorts of each type");
         long beforeWarmup = System.currentTimeMillis();
-        int warmupMax = warmupNumber * 2;
+        int warmupMax = warmupRuns * 2;
         for (int i = 0; i < warmupMax; i += 2) {
-            Arrays.sort(copies[i]);
-            Arrays.parallelSort(copies[warmupNumber + i]);
+            baseline.accept(copies[i]);
+            test.accept(copies[warmupRuns + i]);
         }
         System.out.println("Finished warmup after " + (System.currentTimeMillis() - beforeWarmup) / 1000.0 + " seconds");
 
-        System.out.println("Starting " + runNumber + " benchmark runs of each type");
+        System.out.println("Starting " + benchmarkRuns + " benchmark runs of each type");
         long beforeBenchmark = System.currentTimeMillis();
         long normalTime = 0;
         long parallelTime = 0;
-        int runMax = runNumber * 2;
+        int runMax = benchmarkRuns * 2;
         for (int i = 0; i < runMax; i += 2) {
             int normalIndex = warmupMax + i;
             int parallelIndex = normalIndex + 1;
 
             long before = System.currentTimeMillis();
-            Arrays.sort(copies[normalIndex]);
+            baseline.accept(copies[normalIndex]);
             normalTime += (System.currentTimeMillis() - before);
 
             before = System.currentTimeMillis();
-            Arrays.parallelSort(copies[parallelIndex]);
+            test.accept(copies[parallelIndex]);
             parallelTime += (System.currentTimeMillis() - before);
         }
         System.out.println("Finished benchmark after " + (System.currentTimeMillis() - beforeBenchmark) / 1000.0 + " seconds");
-        System.out.println(runNumber + " normal sorts took a total of " + normalTime / 1000.0 + " seconds for an average of " + (normalTime / (runNumber * 1000.0)) + " seconds per sort");
-        System.out.println(runNumber + " parallel sorts took a total of " + parallelTime / 1000.0 + " seconds for an average of " + (parallelTime / (runNumber * 1000.0)) + " seconds per sort");
+        System.out.println(benchmarkRuns + " baseline runs took a total of " + normalTime / 1000.0 + " seconds for an average of " + (normalTime / (benchmarkRuns * 1000.0)) + " seconds per sort");
+        System.out.println(benchmarkRuns + " test runs took a total of " + parallelTime / 1000.0 + " seconds for an average of " + (parallelTime / (benchmarkRuns * 1000.0)) + " seconds per sort");
     }
 }
