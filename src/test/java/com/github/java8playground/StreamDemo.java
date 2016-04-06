c407246963b108d25473465f91fc7e748964f334
brosander <brosander@pentaho.com>
2016-04-06 11:30:52 -0400

    adding mapreduce demo

--- src/test/java/com/github/java8playground/StreamDemo.java
+++ src/test/java/com/github/java8playground/StreamDemo.java
@@ -2,11 +2,13 @@ package com.github.java8playground;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.junit.Assert.assertEquals;
 
@@ -68,6 +70,105 @@ public class StreamDemo {
         List<User> usersStreaming = names.stream().map(User::create).sorted().collect(Collectors.toList());
 
         assertEquals(usersIterative, usersStreaming);
+    }
+
+    private Stream<String> readIlliad() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("Illiad.txt")));
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(new Iterator<String>() {
+            String next = bufferedReader.readLine();
+
+            @Override
+            public boolean hasNext() {
+                return next != null;
+            }
+
+            @Override
+            public String next() {
+                String result = next;
+                try {
+                    next = bufferedReader.readLine();
+                } catch (IOException e) {
+                    next = null;
+                    e.printStackTrace();
+                }
+                return result;
+            }
+        }, Spliterator.ORDERED), false).onClose(() -> {
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                // Ignore
+            }
+        });
+    }
+
+    @Test
+    public void testWordCount() throws IOException {
+        // Iterative
+        Map<String, Integer> iterativeResult = new HashMap<>();
+        for (String line : readIlliad().collect(Collectors.toList())) {
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                String trimmedWord = word.trim().toLowerCase();
+                if (trimmedWord.length() > 0) {
+                    Integer original = iterativeResult.get(trimmedWord.toLowerCase());
+                    if (original == null) {
+                        iterativeResult.put(trimmedWord.toLowerCase(), 1);
+                    } else {
+                        iterativeResult.put(trimmedWord.toLowerCase(), original + 1);
+                    }
+                }
+            }
+        }
+
+        // Stream
+        Map<String, Integer> streamResult = readIlliad()
+                .parallel()
+                .flatMap(s -> Arrays.stream(s.trim().split("\\s+")))
+                .filter(s -> s.trim().length() > 0)
+                .map(s -> Collections.singletonMap(s.trim().toLowerCase(), 1))
+                .sequential()
+                .reduce(new HashMap<>(), (partialResult, nextElement) -> {
+                    nextElement.entrySet().forEach(nextElementEntry -> partialResult.merge(nextElementEntry.getKey(), nextElementEntry.getValue(), Integer::sum));
+                    return partialResult;
+                });
+
+        // Print results
+        streamResult.entrySet().stream().sorted((o1, o2) -> {
+            int i = o2.getValue() - o1.getValue();
+            if (i != 0) {
+                return i;
+            }
+            return o1.getKey().compareTo(o2.getKey());
+        }).limit(50).forEachOrdered(stringIntegerEntry -> System.out.println(stringIntegerEntry.getKey() + " -> " + stringIntegerEntry.getValue()));
+
+        // Make sure they're the same
+        assertEquals(iterativeResult, streamResult);
+    }
+
+    public static class ImmutableEntry<K, V> implements Map.Entry<K, V> {
+        private final K key;
+        private final V value;
+
+        public ImmutableEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            throw new UnsupportedOperationException("Immutable entry");
+        }
     }
 
     public static class User implements Comparable<User> {
